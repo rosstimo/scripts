6fccf03cd8e396433bd3cb5900eaaef0876d96b8
rosstimo <rosstimo@isu.edu>
2023-07-30 06:33:16 -0600

    add sript to toggle on/off kmonad

---
+++ kcheck.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#this script will check if kmonad is running. If not, start it. if yes kill all instances.
+
+if pgrep -x "kmonad" > /dev/null
+then
+    echo "kmonad is running"
+    pkill kmonad
+    echo "kmonad killed"
+else
+    echo "kmonad is not running"
+    kmonad ~/.config/kmonad/tim-fw-test.kbd &
+    echo "kmonad started"
+fi
