b8ae3b35dadcd2c5aba66af8a1d9291c49a2b2f5
rosstimo <rosstimo@isu.edu>
2024-08-13 10:21:08 -0600

    make mountCloud more general

--- mountCloud.sh
+++ mountCloud.sh
@@ -1,39 +1,18 @@
 #!/bin/sh
-#TODO skip if mounted, maybe unmount options
-#maybe select options
+# This script automates the process of mounting all locally configured rclone cloud 
+# storage remotes to local directories under ~/cloud. It checks if each remote is already
+# mounted and only mounts those that are not, ensuring easy access to cloud storage 
+# through the local filesystem.
 
-status=0
-#mount cloud drives
-#if the directory already exists check to see if it mounted
-#if it didn't mount then mount it
-#if it did mount do nothing
-#if the directory doesn't exist then create it then mount it
-if [ -d "/home/tim/cloud/gdrivew/" ] ; then
-  if [ ! $(findmnt -rno SOURCE,TARGET "$HOME/cloud/gdrivew") ] ; then
-    rclone mount gdrivew: /home/tim/cloud/gdrivew/ --daemon
+for remote in $(rclone listremotes)
+do
+  remote=${remote%:}
+    mkdir -p "$HOME/cloud/$remote"
+  if [ ! "$(findmnt -rno SOURCE,TARGET "$HOME/cloud/$remote")" ] ; then
+    echo "Mounting $remote to $HOME/cloud/$remote"
+    rclone mount "$remote": "$HOME/cloud/$remote/" --daemon
+  else
+    echo "$remote: $HOME/cloud/$remote"
   fi
-else
-  mkdir -p /home/tim/cloud/gdrivew/
-  rclone mount gdrivew: /home/tim/cloud/gdrivew/ --daemon
-fi
-
-if [ -d "/home/tim/cloud/gdriveh/" ] ; then
-  if [ ! $(findmnt -rno SOURCE,TARGET "$HOME/cloud/gdriveh") ] ; then
-    rclone mount gdriveh: /home/tim/cloud/gdriveh/ --daemon
-  fi
-else
-  mkdir -p /home/tim/cloud/gdriveh/
-  rclone mount gdriveh: /home/tim/cloud/gdriveh/ --daemon
-fi
-
-if [ -d "/home/tim/cloud/onedrive/" ] ; then
-  if [ ! $(findmnt -rno SOURCE,TARGET "$HOME/onedrive") ] ; then
-    rclone mount onedrive: /home/tim/cloud/onedrive --daemon
-  fi
-
-  mkdir -p /home/tim/cloud/onedrive/
-  rclone mount onedrive: /home/tim/cloud/onedrive --daemon
-fi
-
-exit $status
+done
 
