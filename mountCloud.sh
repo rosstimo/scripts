e04fc31ad2b28f7340754642496cf9a76d92959a
rosstimo <rosstimo@isu.edu>
2024-10-27 08:43:51 -0600

    fix mount

--- mountCloud.sh
+++ mountCloud.sh
@@ -3,16 +3,26 @@
 # storage remotes to local directories under ~/cloud. It checks if each remote is already
 # mounted and only mounts those that are not, ensuring easy access to cloud storage 
 # through the local filesystem.
+# Enjoy!
+# Tim R
 
-for remote in $(rclone listremotes)
-do
-  remote=${remote%:}
+# Run the internet connectivity check script before proceeding
+check_internet.sh
+if [ $? -eq 0 ]; then
+  # Code to run if internet is available
+  for remote in $(rclone listremotes)
+  do
+    remote=${remote%:}
     mkdir -p "$HOME/cloud/$remote"
-  if [ ! "$(findmnt -rno SOURCE,TARGET "$HOME/cloud/$remote")" ] ; then
-    echo "Mounting $remote to $HOME/cloud/$remote"
-    rclone mount "$remote": "$HOME/cloud/$remote/" --daemon
-  else
-    echo "$remote: $HOME/cloud/$remote"
-  fi
-done
-
+    if [ ! "$(findmnt -rno SOURCE,TARGET "$HOME/cloud/$remote")" ] ; then
+      echo "Mounting $remote to $HOME/cloud/$remote"
+      notify-send  "Mounting $remote to $HOME/cloud/$remote" &
+      rclone mount "$remote": "$HOME/cloud/$remote/" --daemon
+    else
+      echo "$remote: $HOME/cloud/$remote"
+    fi
+  done
+else
+  # Send a notification if internet is not available
+  notify-send "Internet Connectivity" "No Internet connection detected. Unable to mount cloud storage." &
+fi
